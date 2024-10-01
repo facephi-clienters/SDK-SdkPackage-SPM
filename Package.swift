0e7afd5c00719aec647e91ac268ab4fc32309ef3
Facephi <developer@facephi.com>
2024-10-01 18:58:54 +0200

    New SPM Version: 2.2.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .upToNextMinor(from: "2.1.0"))
-        .package(url: "git@github.com:facephi-clienters/SDK-LicensingPackage-SPM.git", .upToNextMinor(from: "2.1.0"))
+        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .upToNextMinor(from: "2.2.0"))
+        .package(url: "git@github.com:facephi-clienters/SDK-LicensingPackage-SPM.git", .upToNextMinor(from: "2.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
