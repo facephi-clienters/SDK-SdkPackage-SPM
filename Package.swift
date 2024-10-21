0e1515775b960a039b4db352f650101f8d209219
Jorge Poveda <jpoveda@facephi.com>
2024-10-21 09:32:22 +0200

    [SM-4054] fix: Mistake in Package-template created an invalid manifest

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .upToNextMinor(from: "2.0.0"))
+        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .upToNextMinor(from: "2.0.0")),
         .package(url: "git@github.com:facephi-clienters/SDK-LicensingPackage-SPM.git", .upToNextMinor(from: "2.0.0"))
     ],
     targets: [
