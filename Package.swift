790dad0b70428029719f650ea4e4dd5e00949818
Facephi <developer@facephi.com>
2026-06-01 17:25:25 +0200

    New SPM Version: 2.8.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.8.0")),
+        .package(url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.8.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,7 +24,7 @@ let package = Package(
             name: "SDK-SdkPackage-SPM",
             dependencies: ["SDK-CorePackage-SPM", "sdk"]),
         .binaryTarget(name: "sdk",
-        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/SDK/FPHISDKMainComponent/2.8.0/sdk.zip",
-        checksum: "8f17fb5413f41a31515ad08dcefb15926df0975e048b6b5ae71efb677fd98294"),
+        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/SDK/FPHISDKMainComponent/2.8.1/sdk.zip",
+        checksum: "7b214bcd6927b6aead8da9ddfd2c7468da77d28092849f082d692e806283de4e"),
     ]
 )
